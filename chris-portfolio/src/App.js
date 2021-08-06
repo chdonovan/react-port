82537858aca3309bafd82c75550171374d6794a2
chdonovan <chrisdonovan311@gmail.com>
2021-08-05 21:48:39 -0500

    contact section added

--- chris-portfolio/src/App.js
+++ chris-portfolio/src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Porfolio from "./components/Portfolio";
+import ContactUs from "./components/Contact";
 
 class App extends Component {
   render(){
@@ -10,6 +11,7 @@ class App extends Component {
         <Header/>
         <About/>
         <Porfolio/>
+        <ContactUs/>
       </div>
     )
   }
---
+++ chris-portfolio/src/components/Contact.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me for any work or suggestions below
+              </p>
+            </div>
+          </div>
+          <div className="row">
+              
+                <h4>Linked in :
+                  
+                </h4>
+              </div>
+            
+          
+        </section>
+        );
+  }
+}
