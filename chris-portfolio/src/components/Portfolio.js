64958bd7e3a7604df0c7d6ede73b4e53792c5258
chdonovan <chrisdonovan311@gmail.com>
2021-08-08 11:08:41 -0500

    images for port

--- chris-portfolio/src/components/Portfolio.js
+++ chris-portfolio/src/components/Portfolio.js
@@ -12,18 +12,23 @@ export default class Porfolio extends Component {
                 <div className="works-cards">
                     <div className="card">
                         <h4>KnowGo</h4>
+                        <img className="workspic" src="images/knowgo.png"></img>
                     </div>
                     <div className="card">
                         <h4>The Collectors Vault</h4>
+                        <img className="workspic" src="images/collect.png"></img>
                     </div>
                     <div className="card">
                         <h4>README generator</h4>
+                        <img className="workspic" src="images/readmescrn.png"></img>
                     </div>
                     <div className="card">
                         <h4>Social API</h4>
+                        <img className="workspic" src="images/reactiondelete.gif"></img>
                     </div>
                     <div className="card">
                         <h4>Budget Tracker</h4>
+                        <img className="workspic" src="images/budget-scrn.png"></img>
                     </div>
                     <div className="card">
                         <h4>Team-Profile-Generator</h4>
