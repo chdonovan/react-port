1ef57dc17fd15989b9dd299550078866d01996d5
chdonovan <chrisdonovan311@gmail.com>
2021-08-08 12:12:50 -0500

    contact form in place

--- chris-portfolio/src/components/Contact.js
+++ chris-portfolio/src/components/Contact.js
@@ -3,19 +3,33 @@ export default class ContactUs extends Component {
   render() {
     return (
       <section id="contact">
-          <div className="row section-head">
-            <div className="ten columns">
-              <p className="lead">
+        <h1> contact me 
+        
+        </h1>
+        <p>
               Feel free to contact me for any work or suggestions below
               </p>
-            </div>
-          </div>
-          <div className="row">
+        
+          <div className="contact-sec">
+          
+              <form className="form">
+                <label> Name:
+                  <input type="text"></input>
+                </label>
+                <label>Email:
+                  <input type="text"></input>
+                
+                </label>
+                <label> Message:
+                  <input type="text"></input>
+                </label>
+                <input type ="submit" value="Submit"></input> 
+              </form>
+          
               
-                <h4>Linked in :
-                  
-                </h4>
-              </div>
+            
+          </div>
+          
             
           
         </section>
