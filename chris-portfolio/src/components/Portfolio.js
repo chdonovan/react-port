22c81ccf56c9b766d146185040e85a68717fa668
chdonovan <chrisdonovan311@gmail.com>
2021-08-08 10:57:43 -0500

    portfolio styles started

--- chris-portfolio/src/components/Portfolio.js
+++ chris-portfolio/src/components/Portfolio.js
@@ -6,8 +6,29 @@ export default class Porfolio extends Component {
             <div className="row">
                 
                 <h1>Check Out Some of My Work!</h1>
-                <p>here you will find some examples of my work. </p>
                 
+                
+                </div>
+                <div className="works-cards">
+                    <div className="card">
+                        <h4>KnowGo</h4>
+                    </div>
+                    <div className="card">
+                        <h4>The Collectors Vault</h4>
+                    </div>
+                    <div className="card">
+                        <h4>README generator</h4>
+                    </div>
+                    <div className="card">
+                        <h4>Social API</h4>
+                    </div>
+                    <div className="card">
+                        <h4>Budget Tracker</h4>
+                    </div>
+                    <div className="card">
+                        <h4>Team-Profile-Generator</h4>
+                        <img className="workspic" src="images/team-Screenshot.png"></img>
+                    </div>
                 </div>
                 
             
