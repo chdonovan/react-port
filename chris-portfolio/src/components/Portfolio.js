8a5bdae1125597ba6675bab0819693351dae25a1
chdonovan <chrisdonovan311@gmail.com>
2021-08-08 11:33:32 -0500

    links open in new tab

--- chris-portfolio/src/components/Portfolio.js
+++ chris-portfolio/src/components/Portfolio.js
@@ -11,28 +11,42 @@ export default class Porfolio extends Component {
                 </div>
                 <div className="works-cards">
                     <div className="card">
-                        <h4>KnowGo</h4>
-                        <img className="workspic" src="images/knowgo.png"></img>
+                        <h4>KnowGO</h4>
+                        <a href ="https://chrisdocs.github.io/KnowGo/"
+                        target="_blank"><img className="workspic" src="images/knowgo.png"></img></a>
+                        
                     </div>
                     <div className="card">
                         <h4>The Collectors Vault</h4>
-                        <img className="workspic" src="images/collect.png"></img>
+                        <a href ="https://the-collectors-vault.herokuapp.com/" target="_blank"
+                        ><img className="workspic" src="images/collect.png"></img></a>
+                        
                     </div>
                     <div className="card">
                         <h4>README generator</h4>
-                        <img className="workspic" src="images/readmescrn.png"></img>
+                        <a href ="https://github.com/chdonovan/readme-gen" target="_blank"
+                        ><img className="workspic" src="images/readmescrn.png"></img></a>
+                        
                     </div>
                     <div className="card">
                         <h4>Social API</h4>
-                        <img className="workspic" src="images/reactiondelete.gif"></img>
+                        <a href ="https://github.com/chdonovan/socialapi" target="_blank"
+                        ><img className="workspic" src="images/reactiondelete.gif"></img></a>
+                        
                     </div>
                     <div className="card">
                         <h4>Budget Tracker</h4>
-                        <img className="workspic" src="images/budget-scrn.png"></img>
+                        <a href ="https://budget--chdonovan.herokuapp.com/" target="_blank">
+                            <img className="workspic" src="images/budget-scrn.png"></img></a>
+                        
                     </div>
                     <div className="card">
+                        
                         <h4>Team-Profile-Generator</h4>
-                        <img className="workspic" src="images/team-Screenshot.png"></img>
+                        <a href ="https://github.com/chdonovan/Team-Profile-Generator" target="_blank">
+                            <img className="workspic" src="images/team-Screenshot.png"></img>
+                            </a>
+                        
                     </div>
                 </div>
                 
